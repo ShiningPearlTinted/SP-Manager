28fddd5330794052cb5fe150b104ed8c65215a31
ShiningPearlTinted <shiningpearltinted@gmail.com>
2026-09-21 20:26:55 +0800

    Delete agent/start-fastreport.bat

--- agent/start-fastreport.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0.."
-set "PS=%SystemRoot%\SysWOW64\WindowsPowerShell\v1.0\powershell.exe"
-start "" "%PS%" -NoProfile -NonInteractive -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0fastreport-price-tags.ps1"
-exit /b 0
