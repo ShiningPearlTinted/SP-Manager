3b93346fef8470cc39bebd39fc0d0c6358caa4d0
ShiningPearlTinted <shiningpearltinted@gmail.com>
2026-09-21 20:26:46 +0800

    Delete agent/start-fastreport.ps1

--- agent/start-fastreport.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference='Stop'
-$root=$PSScriptRoot
-$script=Join-Path $root 'fastreport-price-tags.ps1'
-$ps=Join-Path $env:SystemRoot 'SysWOW64\WindowsPowerShell\v1.0\powershell.exe'
-if(!(Test-Path $ps)){$ps=Join-Path $env:SystemRoot 'System32\WindowsPowerShell\v1.0\powershell.exe'}
-& $ps -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $script
