7671ca0f8c2f8d3a5ac6092963b36d778f557349
ShiningPearlTinted <shiningpearltinted@gmail.com>
2026-09-25 14:03:00 +0800

    Delete RESTART-SP-MANAGER-AGENTS.bat

--- RESTART-SP-MANAGER-AGENTS.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set "PS_EXE=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
-echo Stopping old SP-Manager agents...
-for /f "tokens=5" %%P in ('netstat -ano ^| findstr ":18765 .*LISTENING"') do taskkill /PID %%P /F >nul 2>nul
-ping 127.0.0.1 -n 2 >nul
-call "%~dp0agent\install-windows.bat"
